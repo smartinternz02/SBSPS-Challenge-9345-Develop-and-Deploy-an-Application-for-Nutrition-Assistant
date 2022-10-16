256beab78a0b8f7587e7c0de362810fea8508e5d
M S I <healthBiyanite89>
2022-10-16 18:04:16 +0530

    Develop and Deploy an Application for Nutrition Assistant

---
+++ app/src/main/java/com/example/nutritech/DietChart.java
@@ -0,0 +1,50 @@
+package com.example.nutritech;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DietChart extends AppCompatActivity {
+
+    Button b1,b2,b3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diet_chart);
+
+        b1 = (Button) findViewById(R.id.button1);
+        b2 = (Button) findViewById(R.id.button2);
+        b3 = (Button) findViewById(R.id.button3);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i1 = new Intent(getApplicationContext(), SetAlarm.class);
+                startActivity(i1);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i1 = new Intent(getApplicationContext(), SetAlarm.class);
+                startActivity(i1);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i1 = new Intent(getApplicationContext(), SetAlarm.class);
+                startActivity(i1);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/nutritech/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.nutritech;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class MainActivity extends AppCompatActivity {
+    private static int splash_time_out = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                    Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+                    startActivity(intent);
+                    finish();
+            }
+        },splash_time_out);
+    }
+}
